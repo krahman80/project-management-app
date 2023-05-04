eb5cbe79b4e19de9c808ec79ef0d2b9f47d481d6
Krahman <kautsar.rahman@gmail.com>
2023-05-04 14:24:50 +0900

    update to laravel 8

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,19 +1,6 @@
 window._ = require('lodash');
 
 /**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-// try {
-//     window.Popper = require('popper.js').default;
-//     window.$ = window.jQuery = require('jquery');
-
-//     require('bootstrap');
-// } catch (e) {}
-
-/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
@@ -22,10 +9,6 @@ window._ = require('lodash');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-import 'bootstrap/js/dist/collapse';
-import 'bootstrap/js/dist/dropdown';
-import 'bootstrap/js/dist/tab';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -36,8 +19,6 @@ import 'bootstrap/js/dist/tab';
 // import Echo from 'laravel-echo';
 
 // window.Pusher = require('pusher-js');
-
-const flatpickr = require('flatpickr');
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
--- routes/web.php
+++ routes/web.php
@@ -13,44 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'Auth\LoginController@showLoginForm');
-
-Auth::routes(['register' => false, 'reset' => false]);
-
-Route::middleware('auth')->prefix('admin')->namespace('Admin')->group(function() {
-    Route::get('roles', 'RolesController@index')->name('admin.roles.index');
-    Route::get('roles/create', 'RolesController@create')->name('admin.roles.create');
-    Route::post('roles', 'RolesController@store')->name('admin.roles.store');
-    Route::get('roles/{id}/edit', 'RolesController@edit')->name('admin.roles.edit');
-    Route::patch('roles/{id}', 'RolesController@update')->name('admin.roles.update');
-
-    Route::get('permissions', 'PermissionsController@index')->name('admin.permissions.index');
-    Route::get('permissions/create', 'PermissionsController@create')->name('admin.permissions.create');
-    Route::post('permissions', 'PermissionsController@store')->name('admin.permissions.store');
-    Route::get('permissions/{id}/edit', 'PermissionsController@edit')->name('admin.permissions.edit');
-    Route::patch('permission/{id}', 'PermissionsController@update')->name('admin.permissions.update');
-    Route::delete('permision/{id}', 'PermissionsController@destroy')->name('admin.permissions.delete');
-
-    Route::get('user', 'UsersController@index')->name('admin.users.index');
-    Route::get('user/assign/{id}', 'UsersController@assignUser')->name('admin.roles.assign');
-    Route::post('user/store', 'UsersController@storeAssignUser')->name('admin.roles.store.assign');
-    
+Route::get('/', function () {
+    return view('welcome');
 });
-
-Route::middleware('auth')->prefix('user')->namespace('User')->group(function() {
-    Route::get('my-task', 'TaskController@myTask')->name('user.mytask');
-    Route::get('my-task/{id}','TaskController@show')->name('user.mytask.detail');
-    Route::patch('my-task/{id}', 'TaskController@update')->name('user.mytask.update');
-    Route::post('my-task/search','TaskController@search')->name('user.mytask.search');
-    Route::post('comment/store', 'CommentController@store')->name('comment.store');
-
-});
-
-Route::middleware('auth')->prefix('project')->namespace('Project')->group(function() {
-    Route::get('project', 'ProjectController@index')->name('project.index');
-    Route::get('project/{id}', 'ProjectController@show')->name('project.show');
-    Route::patch('project/{id}', 'ProjectController@update')->name('project.update');
-    Route::get('task/{id}', 'TaskController@show')->name('project.task.show');
-});
-
-Route::get('/home', 'HomeController@index')->name('home');
